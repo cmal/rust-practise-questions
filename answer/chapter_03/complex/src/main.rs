3309d831677a761e4342ef26499517e861cc47ef
Yu Zhao <{ID}+{username}@users.noreply.github.com>
2020-05-29 20:40:32 +0800

    feat: +-* for complex, v2, change struct

--- answer/chapter_03/complex/src/main.rs
+++ answer/chapter_03/complex/src/main.rs
@@ -2,14 +2,11 @@ use std::ops::{Add, Mul, Sub};
 use std::fmt;
 
 #[derive(Copy, Clone)]
-struct Complex {
-    r: i32,
-    v: i32
-}
+struct Complex (i32, i32);
 
 impl fmt::Display for Complex {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "( {:3},{:3} j )", self.r, self.v)
+        write!(f, "( {:3},{:3} j )", self.0, self.1)
     }
 }
 
@@ -17,10 +14,7 @@ impl Add<Complex> for Complex {
     type Output = Complex;
 
     fn add(self, rhs: Complex) -> Complex {
-        Complex {
-            r: self.r + rhs.r,
-            v: self.v + rhs.v
-        }
+        Complex (self.0 + rhs.0, self.1 + rhs.1)
     }
 }
 
@@ -28,10 +22,7 @@ impl Sub<Complex> for Complex {
     type Output = Complex;
 
     fn sub(self, rhs: Complex) -> Complex {
-        Complex {
-            r: self.r - rhs.r,
-            v: self.v - rhs.v
-        }
+        Complex (self.0 - rhs.0, self.1 - rhs.1)
     }
 }
 
@@ -39,31 +30,17 @@ impl Mul<Complex> for Complex {
     type Output = Complex;
 
     fn mul(self, rhs: Complex) -> Complex {
-        Complex {
-            r: self.r * rhs.r - self.v * rhs.v,
-            v: self.r * rhs.v + self.v * rhs.r
-        }
+        Complex (self.0 * rhs.0 - self.1 * rhs.1, self.0 * rhs.1 + self.1 * rhs.0)
     }
 }
 
 fn main() {
-    let c1 = Complex {
-        r: 2,
-        v: 3
-    };
-    let c2 = Complex {
-        r: -4,
-        v: 7
-    };
+    let c1 = Complex(2, 3);
+    let c2 = Complex(-4, 7);
 
-    let c3 = Complex {
-        r: 0,
-        v: 5
-    };
-    let c4 = Complex {
-        r: 11,
-        v: 0
-    };
+    let c3 = Complex(0, 5);
+    let c4 = Complex(11, 0);
+
     println!("{} + {} = {}", c1, c2, c1 + c2);
     println!("{} * {} = {}", c1, c3, c1 * c3);
     println!("{} - {} = {}", c4, c2, c4 - c2);
