ad2c514fd0fdbc3940f5b84f0af15292ee4e6123
Yu Zhao <{ID}+{username}@users.noreply.github.com>
2020-05-29 20:06:01 +0800

    fix: remove commnets

--- answer/chapter_03/compare_dates/src/main.rs
+++ answer/chapter_03/compare_dates/src/main.rs
@@ -10,48 +10,14 @@ struct Date {
     day: String
 }
 
-// struct ParseError {}
-
-// type DateResult = Result<Date, ParseError>;
-
 fn to_date(date: &str) -> Date {
     let re = Regex::new(r"(?P<y>\d{4})\D*(?P<m>\d{2})\D*(?P<d>\d{2})").unwrap();
-
     let caps = re.captures(date).unwrap();
     Date {
         year: caps["y"].to_string(),
         month: caps["m"].to_string(),
         day: caps["d"].to_string()
     }
-    // let digit = Regex::new(r"\d").unwrap();
-    // let nonDigits = digit.replace_all(&date, |caps: &Captures| {
-    //     caps.at(1).unwrap().to_owned()
-    // });
-    // let mut date1 = "";
-    // if nonDigits.len() > 0 {
-    //     date1 = Regex::new(&nonDigits[0..1]).replace_all(&date);
-    // } else {
-    //     date1 = date;
-    // }
-
-    // if date.len() != 6 {
-    //     Err(ParseError{})
-    // } else {
-    //     Ok(Date {
-    //         year: date1[0..4].to_string(),
-    //         month: date1[4..6].to_string(),
-    //         day: date[6..].to_string()
-    //     })
-    // }
-
-    // let re = Regex::new(r"\d+").unwrap();
-    // re.replace_all(&date, |caps: &Captures| {
-    //     Date {
-    //         year: caps.at(0).unwrap().to_owned(),
-    //         month: caps.at(1).unwrap().to_owned(),
-    //         day: caps.at(2).unwrap().to_owned()
-    //     }
-    // });
 }
 
 fn main() {
